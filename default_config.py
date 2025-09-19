3bc0782222ec17d823ab52a7592f48ebd8ecf821
esdmr <esdmr0@gmail.com>
2025-09-19 16:25:20 +0330

    Add file

--- default_config.py
+++ default_config.py
@@ -1,3 +1,6 @@
 from secrets import token_urlsafe
+from pathlib import Path
 
 SECRET_KEY = token_urlsafe(64)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(Path(__file__).parent.joinpath('app.db').absolute())
